e316af5c9aff2156b6f09dcc6463767de656941a
maksimilyan.lamaev.13@inbox.ru <maksimilyan.lamaev.13@inbox.ru>
2023-01-19 09:57:11 +0700

    Практическое задание №2. Вариант 12

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите x");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите y");
+double y = Convert.ToDouble(Console.ReadLine());
+Plane plane = new(x, y);
+Console.WriteLine(plane.distance());
+sealed class Plane
+{
+    private readonly double first;
+    private readonly double second;
+  
+    public Plane(double a, double c)
+    {
+        first = a;
+        second = c;
+    }
+    public double First
+    {
+        get => first;
+    }
+    public double Second
+    {
+        get => second;
+    }
+    public double distance()
+        => second - first;
+}
 
